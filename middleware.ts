c4761671dcf2626e0925c75cc401707a1f9a931b
Th4phat <sthaphach@gmail.com>
2025-07-05 16:19:14 +0700

    tryna fix sum3

--- middleware.ts
+++ middleware.ts
@@ -3,22 +3,25 @@ import {
   createRouteMatcher,
   nextjsMiddlewareRedirect,
 } from "@convex-dev/auth/nextjs/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const isSignInPage = createRouteMatcher(["/signin"]);
 const isProtectedRoute = createRouteMatcher(["/", "/server", "/dashboard"]);
 
-export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
+// export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
+//   console.log("Middleware is running for path:", request.nextUrl.pathname);
+//   // if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
+//   //   return nextjsMiddlewareRedirect(request, "/dashboard");
+//   // }
+//   // if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
+//   //   return nextjsMiddlewareRedirect(request, "/signin");
+//   // }
+//   return NextResponse.next();
+// });
+export function middleware(request: NextRequest) {
   console.log("Middleware is running for path:", request.nextUrl.pathname);
-  // if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
-  //   return nextjsMiddlewareRedirect(request, "/dashboard");
-  // }
-  // if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
-  //   return nextjsMiddlewareRedirect(request, "/signin");
-  // }
   return NextResponse.next();
-});
-
+}
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 }
